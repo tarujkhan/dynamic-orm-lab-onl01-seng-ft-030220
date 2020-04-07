ba57a80ccfa2d2b6b77e948a0413e1759cd51f85
taroj khan <tarujkhan@yahoo.com>
2020-04-07 16:52:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
 
 
 def self.find_by(attribute_hash)
-  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{attribute_hash.values.first}")
+  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{attribute_hash.keys.first}' = '#{attribute_hash.values.first}'")
 end
 
 end
